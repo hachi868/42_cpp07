8157986c9e54920673fc7da4bbb30dd007431570
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-03-20 14:20:05 +0900

    :dizzy:[update]ex01 Arr def

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,6 +1,27 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+template <typename T> class Array {
+public:
+	Array(){
 
+	};
+	Array(unsigned int n) {
+
+	};
+	Array(const Array &obj) {
+
+	};
+	Array &operator = (const Array &obj) {
+
+	};
+	~Array(){
+
+	};
+private:
+	size_t	size() const {
+
+	};
+}
 
 #endif //ITER_HPP
