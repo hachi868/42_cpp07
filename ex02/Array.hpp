f86bdf19e001532725963cb7d891e491220e8561
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-03-20 16:26:01 +0900

    :dizzy:[update]ex01 Class Array

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -3,25 +3,34 @@
 
 template <typename T> class Array {
 public:
-	Array(){
-
-	};
-	Array(unsigned int n) {
-
-	};
-	Array(const Array &obj) {
-
+	Array() : size_(0), ar_(new T){};//new T[0]?
+	Array(unsigned int n) : size_(n), ar_(new T[n]) {};
+	Array(const Array &obj) : size_(obj.size()), ar_(obj.copyAr()) {
 	};
 	Array &operator = (const Array &obj) {
-
+		if (this != &obj) {
+			this->size_ = obj.size();
+			delete [] this->ar_;
+			this->ar_ = obj.copyAr();
+		}
+		return (*this);
 	};
 	~Array(){
-
+		delete [] ar_;
 	};
+	unsigned int	size() const {
+		return size_;
+	};
+	T *copyAr() {
+		T * arDst = new T[size_];
+		for (unsigned int i=0;i<size_;i++;) {
+			arDst[i] = ar_[i];
+		}
+		return (arDst);
+	}
 private:
-	size_t	size() const {
-
-	};
+	T				*ar_;
+	unsigned int	size_;
 }
 
 #endif //ITER_HPP
