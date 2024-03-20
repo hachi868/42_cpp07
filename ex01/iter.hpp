48135361b3847bfc0b6d300227814939f4a1002d
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-03-20 20:56:10 +0900

    :dizzy:[update]ex01 overload, test

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,8 +1,17 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T> void iter(T *ar, size_t len, void (*f)(T)) {
-	for(size_t i=0; i<len; i++)
+#include <iostream>
+
+template <typename T> void iter(const T *ar, unsigned int len, void (*f)(const T &)) {
+	std::cout << "-*-*- iter const -*-*-" << std::endl;
+	for(unsigned int i=0; i<len; i++)
+		f(ar[i]);
+}
+
+template <typename T> void iter(T *ar, unsigned int len, void (*f)(T &)) {
+	std::cout << "-*-*- iter no-const -*-*-" << std::endl;
+	for(unsigned int i=0; i<len; i++)
 		f(ar[i]);
 }
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,13 +1,33 @@
 #include "iter.hpp"
-#include <iostream>
 
-__attribute__((destructor))
-static void destructor() {
-	system("leaks -q ex01");
+//__attribute__((destructor))
+//static void destructor() {
+//	system("leaks -q ex01");
+//}
+
+template <typename T> void display(T &a) {
+	std::cout << "[ref] " << a << std::endl;
+}
+
+template <typename T> void display(const T &a) {
+	std::cout << "[const:ref] " << a << std::endl;
 }
 
 int main()
 {
+	std::string arStr[] = { "str0", "str1", "str2", "str3", "str4", "str5", "str6"};
+	const std::string arStrConst[] = { "str0", "str1", "str2", "str3", "str4", "str5", "str6"};
+	const int arIntConst[] = { 5, 4, 3, 2, 1, 0};
+
+	//len取得
+	unsigned int lenArStr = sizeof(arStr) / sizeof(arStr[0]);
+	unsigned int lenArStrConst = sizeof(arStrConst) / sizeof(arStrConst[0]);
+	unsigned int lenArIntConst = sizeof(arIntConst) / sizeof(arIntConst[0]);
+
+	//iter
+	iter<std::string>(arStr, lenArStr, display<std::string>);
+	iter<const std::string>(arStrConst, lenArStrConst, display<const std::string>);
+	iter<const int>(arIntConst, lenArIntConst, display<const int>);
 
 	return (0);
 }
